2d9a3424a6ae191e4561e0f9831347fcf322c0f1
Nithya Rajaram <nithya@supportbee.com>
2012-04-20 17:41:27 -0400

    exp

--- app.rb
+++ app.rb
@@ -2,12 +2,11 @@ require 'sinatra'
 require 'json'
 
 post '/' do
- @profile = JSON.parse(request.body.read.to_s)
- "profile"
+ profile = JSON.parse(request.body.read.to_s)
+ "#{profile.inspect}"
 end
 
 get '/' do
   "Hello World"
-  "#{@profile}"
 end
 
