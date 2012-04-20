f903b0811dfb9422fec546e1a2d5bec74309a386
Nithya Rajaram <nithya@supportbee.com>
2012-04-20 19:16:01 -0400

    checking sms sending

--- app.rb
+++ app.rb
@@ -1,21 +1,19 @@
 require 'sinatra'
 require 'json'
+require 'rubygems'
+require 'net/http'
+require 'uri'
 
 post '/' do
       
-      json = JSON.parse(request.body.read.to_s)
-      payload = json['payload']
-      puts "#{payload.inspect}"
-      
+      #json = JSON.parse(request.body.read.to_s)
+      #payload = json['payload']
+      #puts "#{payload.inspect}"
+            
+url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
+msg = URI::encode("This is a new ticket")
+params = {'username' => 'supportbee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
+page = Net::HTTP.post_form(url,params)
+puts page.body
+
 end
-
-post '/' do
-  "great time"
-end
-
-
-get '/' do
-  "test"
-end
-
-
