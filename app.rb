dbd2fe7700553607b3bc85aabcc6c1a79d224874
Nithya Rajaram <nithya@supportbee.com>
2012-04-20 20:06:50 -0400

    exp

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
 require 'sinatra'
 require 'json'
 require 'rubygems'
-require 'net/http'
-require 'uri'
+
+#require 'net/http'
+#require 'uri'
 
 post '/' do
       
-      #json = JSON.parse(request.body.read.to_s)
-      #payload = json['payload']
-      #puts "#{payload.inspect}"
+      json = JSON.parse(request.body.read.to_s)
+      payload = json['payload']
+      puts "#{payload.inspect}"
             
-url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
-msg = URI::encode("This is a new ticket")
-params = {'username' => 'supportbee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
-page = Net::HTTP.post_form(url,params)
-puts page.body
+#url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
+#msg = URI::encode("This is a new ticket")
+#params = {'username' => 'supportbee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
+#page = Net::HTTP.post_form(url,params)
+#puts page.body
 
 end
