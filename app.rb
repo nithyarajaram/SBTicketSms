684fe6948e439a0093fe3bf7b4e49a4f64190c1c
Nithya Rajaram <nithya@supportbee.com>
2012-04-20 20:31:51 -0400

    exp

--- app.rb
+++ app.rb
@@ -1,21 +1,19 @@
 require 'sinatra'
 require 'json'
 require 'rubygems'
-
-#require 'net/http'
-#require 'uri'
+require 'net/http'
+require 'uri'
 
 post '/' do
       
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
       puts payload.inspect
-      puts "[#{payload['company']['name']}:New Ticket] #{payload['ticket']['subject']}\n https://#{payload['company']['subdomain']}.supportbee.com/tickets/#{payload['ticket']['id']}"
             
-#url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
-#msg = URI::encode("This is a new ticket")
-#params = {'username' => 'supportbee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
-#page = Net::HTTP.post_form(url,params)
-#puts page.body
+url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
+msg = URI::encode("This is a new ticket")
+params = {'username' => 'supportbee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
+page = Net::HTTP.post_form(url,params)
+puts page.body
 
 end
