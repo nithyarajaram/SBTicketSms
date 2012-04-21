633f586531960f04da806189ee848c6ce3db50b1
Nithya Rajaram <nithya@supportbee.com>
2012-04-21 18:14:27 -0400

    some spell correction

--- config/bulksms.rb
+++ config/bulksms.rb
@@ -1,4 +1,4 @@
-Bulksms.configure do|configure|
+Bulksms.configure do|config|
   config.username = ENV["BULKSMS_USERNAME"]
   config.password = ENV["BULKSMS_PASSWORD"]
   config.recipient1 = ENV["BULKSMS_RECEPIENT1"]
