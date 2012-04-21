4a68fdd8648164fb3b3d979a6ff3c3b2b9b08abc
Nithya Rajaram <nithya@supportbee.com>
2012-04-21 16:26:14 -0400

    sms part integrated

--- app.rb
+++ app.rb
@@ -5,15 +5,12 @@ require 'net/http'
 require 'uri'
 
 post '/' do
-      
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
-      puts payload.inspect
-            
-url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
-msg = URI::encode("This is a new ticket")
-params = {'username' => 'supportbee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
-page = Net::HTTP.post_form(url,params)
-puts page.body
-
+      if payload['action_type'] == 'ticket_created'
+         url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
+         msg = URI::encode("[#{payload['company']['name']}:New Ticket] #{payload['ticket']['subject']}")
+         params = {'username' => 'supporee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
+         page = Net::HTTP.post_form(url,params)
+      end
 end
