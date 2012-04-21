804d32df6a116f3a7271787170b6307e3edbdd2e
Nithya Rajaram <nithya@supportbee.com>
2012-04-20 20:23:53 -0400

    exp

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ post '/' do
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
       puts payload.inspect
+      puts "[#{payload['company']['name']}:New Ticket] #{payload['ticket']['subject']}\n https://#{payload['company']['subdomain']}.supportbee.com/tickets/#{payload['ticket']['id']}"
             
 #url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
 #msg = URI::encode("This is a new ticket")
