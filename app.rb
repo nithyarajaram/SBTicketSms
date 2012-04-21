b982d26528b9615e7c5e532ec7cff73971901338
Nithya Rajaram <nithya@supportbee.com>
2012-04-21 16:31:40 -0400

    added inspect

--- app.rb
+++ app.rb
@@ -7,10 +7,12 @@ require 'uri'
 post '/' do
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
+      puts payload.inspect
       if payload['action_type'] == 'ticket_created'
          url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
          msg = URI::encode("[#{payload['company']['name']}:New Ticket] #{payload['ticket']['subject']}")
          params = {'username' => 'supporee', 'password' => 'supbee', 'message' => msg, 'msisdn' => '+841268057258'}
          page = Net::HTTP.post_form(url,params)
+         puts page.body
       end
 end
