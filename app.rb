6fad86cbdb2398e41ce9dbdc1d903cea5d560e55
Nithya Rajaram <nithya@supportbee.com>
2012-04-22 20:56:50 -0400

    Incoming replies and message formatting

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ post '/' do
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
       puts payload.inspect
-      if payload['action_type'] == 'ticket_created'
+      if payload['action_type'] == 'ticket_created' || payload['action_type'] == 'incoming_reply_created'
          url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
-         msg = URI::encode("[#{payload['company']['name']}:New Ticket] #{payload['ticket']['subject']}")
+         msg = CGI::escape("[#{payload['company']['name']}:] #{payload['ticket']['subject']}")
          params = {'username' => ENV["BULKSMS_USERNAME"], 'password' => ENV["BULKSMS_PASSWORD"], 'message' => msg, 'msisdn' => ENV["BULKSMS_RECIPIENT1"]}
          page = Net::HTTP.post_form(url,params)
          puts page.body
