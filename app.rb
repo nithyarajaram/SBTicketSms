9a7d851dc87c04908d67e1a843a6c0399845fb1f
Nithya Rajaram <nithya@supportbee.com>
2012-04-22 23:40:00 -0400

    formatting and messaging done rignt

--- app.rb
+++ app.rb
@@ -9,13 +9,15 @@ post '/' do
       json = JSON.parse(request.body.read.to_s)
       payload = json['payload']
       puts payload.inspect
-      if payload['action_type'] == 'ticket_created' || payload['action_type'] == 'incoming_reply_created'
-         url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
-         #msg = CGI::escape("[#{payload['company']['name']}:] #{payload['ticket']['subject']}")
-         msg = CGI::escape("There is a new ticket")
-         puts msg
-         params = {'username' => ENV["BULKSMS_USERNAME"], 'password' => ENV["BULKSMS_PASSWORD"], 'message' => msg, 'msisdn' => ENV["BULKSMS_RECIPIENT1"]}
-         page = Net::HTTP.post_form(url,params)
-         puts page.body
+      
+      if payload['action_type'] == 'ticket_created'
+        msg = "[#{payload['company']['name']}: New Ticket] #{payload['ticket']['subject']}"
+      elsif  payload['action_type'] == 'incoming_reply_created'
+        msg = "[#{payload['company']['name']}: Reply] #{payload['ticket']['subject']}"
       end
+         
+      url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
+      params = {'username' => ENV["BULKSMS_USERNAME"], 'password' => 'ENV["BULKSMS_PASSWORD"]', 'message' => msg, 'msisdn' => ENV["BULKSMS_RECIPIENT1"]}
+      page = Net::HTTP.post_form(url,params)
+      puts page.body
 end
