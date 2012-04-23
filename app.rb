93ae8bdccdb1dfef6519679d5b5a63e6132009f2
Nithya Rajaram <nithya@supportbee.com>
2012-04-22 22:11:40 -0400

    testing cgi

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ post '/' do
       puts payload.inspect
       if payload['action_type'] == 'ticket_created' || payload['action_type'] == 'incoming_reply_created'
          url = URI("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0")
-         msg = CGI::escape("[#{payload['company']['name']}:] #{payload['ticket']['subject']}")
+         #msg = CGI::escape("[#{payload['company']['name']}:] #{payload['ticket']['subject']}")
+         msg = CGI::escape("There is a new ticket")
          puts msg
          #params = {'username' => ENV["BULKSMS_USERNAME"], 'password' => ENV["BULKSMS_PASSWORD"], 'message' => msg, 'msisdn' => ENV["BULKSMS_RECIPIENT1"]}
          #page = Net::HTTP.post_form(url,params)
